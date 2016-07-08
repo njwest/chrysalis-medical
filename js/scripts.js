de764c6e4b2df3405387785eb6ff2b5c4ae8abec
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2016-07-08 14:40:41 -0400

    Using angular to animate on scroll instead since query was getting too sloppy. Still needs to be fine tuned.

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,66 @@
-$(document).ready(function() {
-
-  var scrollBreakpoint1 = 200;
-  var scrollBreakpoint2 = 400;
-  var scrollBreakpoint3 = 600;
-  var scrollBreakpoint4 = 800;
-  var aboutUs1 = $('#aboutus1').detach();
-
-  // Hide about us descriptions 2 & 3 on page load
-  $('#aboutus2').addClass('hide');
-  $('#aboutus3').addClass('hide');
-
-  // Hide and show aboutus descriptions on scroll
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-
-    /***************
-    SCROLLING DOWN
-    ****************/
-    // Transition from aboutus1 to aboutus2
-    if (scroll >= scrollBreakpoint1) {
-      $('#aboutus1').addClass('fadeOut');
-    }
-
-    if (scroll >= scrollBreakpoint2) {
-      $('#aboutus1').detach();
-      $('#aboutus2').removeClass('hide').addClass('fadeIn');
-    }
-
-    // Transition from aboutus2 to aboutus3
-    if (scroll >= scrollBreakpoint3) {
-      $('#aboutus2').addClass('fadeOut');
-    }
-
-    if (scroll >= scrollBreakpoint4) {
-      $('#aboutus3').removeClass('hide').addClass('fadeIn');
-    }
-
-    /***************
-    SCROLLING UP
-    ****************/
-    // Transition from aboutus3 to aboutus2
-    if (scroll <= scrollBreakpoint4) {
-      $('#aboutus3').removeClass('fadeIn').addClass('hide');
-    }
-
-    if (scroll <= scrollBreakpoint3) {
-      $('#aboutus2').removeClass('fadeOut').addClass('fadeIn');
-    }
-
-    // Transition from aboutus2 to aboutus1
-    if (scroll <= scrollBreakpoint2) {
-      $('#aboutus2').removeClass('fadeIn').addClass('hide');
-    }
-
-    if (scroll <= scrollBreakpoint1) {
-      $('#aboutus').append(aboutUs1);
-      $('#aboutus1').removeClass('fadeOut').addClass('fadeIn');
-    }
-  });
-
-});
+// $(document).ready(function() {
+//
+//   var scrollBreakpoint1 = 300;
+//   var scrollBreakpoint2 = 500;
+//   var scrollBreakpoint3 = 600;
+//   var scrollBreakpoint4 = 900;
+//   var aboutUs1 = $('#aboutus1');
+//   var aboutUs2 = $('#aboutus2');
+//   var aboutUs3 = $('#aboutus3');
+//
+//   // Hide about us descriptions 2 & 3 on page load
+//   $('#aboutus2').addClass('hide');
+//   $('#aboutus3').addClass('hide');
+//
+//   // Hide and show aboutus descriptions on scroll
+//   $(window).scroll(function() {
+//     var scroll = $(window).scrollTop();
+//
+//     /***************
+//     SCROLLING DOWN
+//     ****************/
+//     // Transition from aboutus1 to aboutus2
+//     if (scroll >= scrollBreakpoint1) {
+//       $('#aboutus1').addClass('fadeOut');
+//     }
+//
+//     if (scroll >= scrollBreakpoint2) {
+//       $('#aboutus1').detach();
+//       $('#aboutus2').removeClass('hide').addClass('fadeIn');
+//     }
+//
+//     // Transition from aboutus2 to aboutus3
+//     if (scroll >= scrollBreakpoint3) {
+//       $('#aboutus2').addClass('fadeOut');
+//     }
+//
+//     if (scroll >= scrollBreakpoint4) {
+//       $('#aboutus2').detach();
+//       $('#aboutus3').removeClass('hide').addClass('fadeIn');
+//     }
+//
+//     /***************
+//     SCROLLING UP
+//     ****************/
+//     // Transition from aboutus3 to aboutus2
+//     if (scroll <= scrollBreakpoint4) {
+//       $('#aboutus3').removeClass('fadeIn').addClass('fadeOut');
+//     }
+//
+//     if (scroll <= scrollBreakpoint3) {
+//       $('#aboutus3').detach();
+//       $('#aboutus2').removeClass('fadeOut').addClass('fadeIn');
+//     }
+//
+//     // Transition from aboutus2 to aboutus1
+//     if (scroll <= scrollBreakpoint2) {
+//       $('#aboutus2').removeClass('fadeIn').addClass('hide');
+//     }
+//
+//     if (scroll <= scrollBreakpoint1) {
+//       $('#aboutus').append(aboutUs1);
+//       $('#aboutus1').removeClass('fadeOut').addClass('fadeIn');
+//     }
+//   });
+//
+// });
