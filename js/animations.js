496aeabab869f950fc8a09ffd7f4aa0543b7d7e1
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2016-07-19 09:25:49 -0400

    made animations better.

--- js/animations.js
+++ js/animations.js
@@ -9,33 +9,47 @@ $(document).ready(function() {
   console.log('breakpoint1 =', breakpoint1);
   var breakpoint2 = browserHeight * 2;
   console.log('breakpoint2 =', breakpoint2);
-  // var breakpoint3 = browserHeight;
-  // var breakpoint4 = browserHeight * 1.5;
   var previousScroll = 0;
-  var scrollingUp = 'currently-not-scrolling';
-  var scrollingDown = 'currently-not-scrolling';
   var currentDiv = 'aboutus1';
   var aTop;
-  var scroll = 0;
+  var scroll;
   var directionCheck = function() {
     if (previousScroll < scroll) {
       scrollingDown = true;
       scrollingUp = false;
       previousScroll = scroll;
-      console.log('scrolling down');
     }
     else if (scroll < previousScroll) {
       scrollingDown = false;
       scrollingUp = true;
       previousScroll = scroll;
-      console.log('scrolling up');
     }
   };
+  // Scroll Functions
+  var scrollDownToAboutus2 = function() {
+    $('.aboutus1-text').removeClass('fadeIn').addClass('fadeOut');
+    $('html, body').animate({
+        scrollTop: $("#aboutus2").offset().top
+      }, 1500);
+    $('.aboutus2-text').removeClass('hide fadeOut').addClass('fadeIn').attr('data-wow-delay', '3s');
+    $('#dimer-left').removeClass('hide').addClass('slideInRight pulse infinite');
+  }; // end scrollDownToAboutus2
 
+  var scrollDownToAboutus3 = function() {
+    $('.aboutus2-text').removeClass('fadeIn').addClass('fadeOut');
+    $('html, body').animate({
+        scrollTop: $("#aboutus3").offset().top
+      }, 1500);
+    $('.aboutus3-text').removeClass('hide fadeOut').addClass('fadeIn').attr('data-wow-delay', '3s');
+    $('#dimer-right').removeClass('hide').addClass('slideInRight pulse infinite').attr('data-wow-delay', '3s');
+    $('#dna-green').removeClass('hide').addClass('fadeIn pulse infinite').attr('data-wow-delay', '5s');
+    $('#down-arrow-div').addClass('fadeOut');
+  }; // end scrollDownToAboutus3
 
   // Mouse scroll controller
   $(window).scroll(function() {
     scroll = $(window).scrollTop();
+    console.log('scroll = ', scroll);
 
     // Scrolling Down
     if (currentDiv === 'aboutus1') {
@@ -45,7 +59,6 @@ $(document).ready(function() {
         if($(this).scrollTop()>=aTop){
           scrollDownToAboutus2();
           currentDiv = 'aboutus2 initiated';
-          scrollingDown = 'currently-not-scrolling';
         }
       }
     }
@@ -59,84 +72,18 @@ $(document).ready(function() {
         if ($(this).scrollTop()>=aTop){
           scrollDownToAboutus3();
           currentDiv = 'aboutus3 initiated';
-          scrollingDown = 'currently-not-scrolling';
-        }
-      }
-      else if (scrollingUp) {
-        aTop = ($('#aboutus2').height() / 2);
-        if ($(this).scrollTop()>=aTop){
-          scrollUpToAboutus1();
-          currentDiv = 'aboutus3 initiated';
-          scrollingDown = 'currently-not-scrolling';
         }
       }
     }
-    // Scrolling Up
-    // else if (breakpoint2 <= scroll) {
-    //   currentDiv = 'aboutus3';
-    // }
-    // else if (currentDiv === 'aboutus3') {
-    //   directionCheck();
-    //   if (scrollingUp) {
-    //     scrollUpToAboutus2();
-    //     currentDiv = 'aboutus2 initiated';
-    //   }
-    // }
-    // else if (breakpoint1 < scroll) {
-    //   currentDiv = 'aboutus2';
-    // }
-    // else if (currentDiv === 'aboutus2') {
-    //   directionCheck();
-    //   if (scrollingUp) {
-    //     scrollUpToAboutus1();
-    //     console.log('scrolling up fired');
-    //   }
-    // }
-  });
-
-
-
-
-  // Scroll Functions
-  var scrollDownToAboutus2 = function() {
-    $('.aboutus1-text').addClass('fadeOut');
-    $('html, body').animate({
-        scrollTop: $("#aboutus2").offset().top
-      }, 1500);
-    $('.aboutus2-text').removeClass('hide fadeOut').addClass('fadeIn').attr('data-wow-delay', '1s');
-    $('#dimer-left').removeClass('hide').addClass('slideInRight pulse infinite');
-  }; // and scrollDownToAboutus2
-
-  var scrollDownToAboutus3 = function() {
-    $('.aboutus2-text').removeClass('fadeIn').addClass('fadeOut');
-    $('html, body').animate({
-        scrollTop: $("#aboutus3").offset().top
-      }, 1500);
-    $('.aboutus3-text').removeClass('hide fadeOut').addClass('fadeIn').attr('data-wow-delay', '1s');
-    $('#dimer-right').removeClass('hide').addClass('slideInRight pulse infinite').attr('data-wow-delay', '3s');
-    $('#dna-green').removeClass('hide').addClass('fadeIn pulse infinite').attr('data-wow-delay', '5s');
-    $('#down-arrow-div').addClass('fadeOut');
-  }; // end scrollDownToAboutus3
-
-  var scrollUpToAboutus1 = function() {
-    console.log('scrollUpToAboutus1 fired!')
-    $('.aboutus2-text').addClass('fadeOut');
-    $('html, body').animate({
-        scrollTop: $("#aboutus1").offset().top
-      }, 1500);
-    $('.aboutus1-text').removeClass('hide fadeOut').addClass('fadeIn').attr('data-wow-delay', '1s');
-  }; // and scrollUpToAboutus1
-
-  var scrollUpToAboutus2 = function() {
-    $('.aboutus3-text').addClass('fadeOut');
-    $('html, body').animate({
-        scrollTop: $("#aboutus2").offset().top
-      }, 1500);
-    $('.aboutus2-text').removeClass('hide fadeOut').addClass('fadeIn').attr('data-wow-delay', '1s');
-  }; // and scrollUpToAboutus2
+    if (scroll === 0) {
+      $('.aboutus1-text').removeClass('fadeOut').addClass('fadeIn');
+      $('#down-arrow-div').removeClass('fadeOut').addClass('fadeIn');
+      currentDiv = 'aboutus1';
+      clickCount = 0;
+    }
+  }); // end window scroll function
 
   // Down arrow controller
-  var firstTimeScrolling = true;
   var clickCount = 0;
   $('#down-arrow').on('click', function() {
     clickCount++;
