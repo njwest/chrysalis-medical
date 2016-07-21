76d0c482c871b7619dc7601c763d2698004b7724
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2016-07-21 12:12:08 -0400

    arrow puts in correct position depending if mobile nav drawer is open or not.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
-$(document).ready(function() {
+$(function() {
 
+  // Push body down when mobile nav drawer opens
+  // Use this only if client wants the body of the page to be pushed down as the mobile nav drawer opens.
+  // $('button.navbar-toggle').click(function() {
+  //   if ($('body').css('padding-top') === '80px') {
+  //       $('body').css('padding-top', '+=350');
+  //   } else {
+  //       $('body').css('padding-top', '80px');
+  //   }
+  // });
 
+  // Auto-close mobile nav on user click
+  $('.nav a').on('click', function(){
+    if($('.navbar-toggle').css('display') !='none'){
+      $(".navbar-toggle").trigger( "click" );
+    }
+  });
 
-});
+  // Mobile Nav Arrow Direction
+  $('button.navbar-toggle').click(function() {
+    if ($(this).attr('data-arrow') === 'down') {
+      $(this).attr('data-arrow', 'up');
+      $('#mobile-arrow').removeClass('fa-chevron-down').addClass('fa-chevron-up');
+      console.log('if fired');
+    }
+    else if ($(this).attr('data-arrow') === 'up') {
+      $(this).attr('data-arrow', 'down');
+      $('#mobile-arrow').removeClass('fa-chevron-up').addClass('fa-chevron-down');
+      console.log('else if fired');
+    }
+  });
+
+}); // end document ready function
