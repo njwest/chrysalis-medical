56329f8fc5922d0c213faea77f45c5090f9f42b1
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2016-07-26 16:37:48 -0400

    worked with ohm on the ajax request for the contact form. he needs to do some things in the backend for it to work. will touch base with him tomorrow.

--- js/contactus.js
+++ js/contactus.js
@@ -15,41 +15,46 @@ $(function() {
     console.log('email = ', email);
     console.log('message = ', message);
 
+    var jsondata = {
+        "UserEmail" : email,
+        "UserFirstName" : name,
+        "UserLastName" : "",
+        "FromEmail" : "",
+        "ToEmail" : "Ohmmurugan.Sadasivam@nucleuscentral.com",
+        "ReplyToEmail" : "",
+        "CopyToEmail" : "",
+        "Subject" : "Chrysalis Contacts",
+        "EmailContent" : message
+    };
+
+    console.log(jsondata);
     $.ajax({
-      type: 'POST',
-      url: $(form).attr('action'),
-      data: formData
-    })
+      type: "POST",
+      url: "http://www.chrysalismedical.com/emailapi/api/email",
+      contentType: "application/json; charset=utf-8",
+      data: jsondata,
+      success: function (msg) {
+          $(formMessages).removeClass('error');
+          $(formMessages).addClass('success');
+          $(formMessages).text(response);
+          $('#name').val('');
+          $('#email').val('');
+          $('#message').val('');
+      },
+      error: function (msg) {
+        console.log('error');
+      }
+  });
+});
 
     // if success
-    .done(function(response) {
-      // Make sure that the formMessages div has the 'success' class.
-      $(formMessages).removeClass('error');
-      $(formMessages).addClass('success');
-
-      // Set the message text.
-      $(formMessages).text(response);
-
-      // Clear the form.
-      $('#name').val('');
-      $('#email').val('');
-      $('#message').val('');
-    })
-
-    // if error
-    .fail(function(data) {
-      // Make sure that the formMessages div has the 'error' class.
-      $(formMessages).removeClass('success');
-      $(formMessages).addClass('error');
-
-      // Set the message text.
-      if (data.responseText !== '') {
-          $(formMessages).text(data.responseText);
-      } else {
-          $(formMessages).text('Oops! An error occured and your message could not be sent.');
-      }
-    });
-
-  });
+  //   .done(function(response) {
+  //
+  //   })
+  //
+  //   // if error
+  //
+  //
+  // });
 
 }); // end document ready
