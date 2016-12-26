2f36d99a314425e8f1a8e98e129de8c2af07a1b5
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2016-12-26 18:32:03 -0500

    tidied up code. got rid of console.logs

--- app/js/controllers/navbarController.js
+++ app/js/controllers/navbarController.js
@@ -3,12 +3,13 @@
 chrysalisApp.controller('NavbarController', ['$scope', '$http', '$location', function($scope, $http, $location) {
 
   $http.get('data/navItems.json').success(function(navItemsData) {
+
     $scope.navItems = navItemsData;
 
     // set class='active' when user types in url in address bar
-    $scope.url = $location.$$url; // route with '/'
-    // route name without '/' except for home
-    $scope.fixLink = function() {
+    $scope.url = $location.$$url; // route name with '/'
+
+    $scope.fixLink = function() { // route name without '/' except for home
       if ($scope.url === '/') {
         // for homepage
         $scope.link = '/';
@@ -18,7 +19,9 @@ chrysalisApp.controller('NavbarController', ['$scope', '$http', '$location', fun
         $scope.link = $scope.url.replace('/','');
       }
     };
+
     $scope.fixLink();
+
     for (var i = 0; i < $scope.navItems.length; i++) {
       if ($scope.navItems[i].link === $scope.link) {
         $scope.navItems[i].active = true;
--- app/js/controllers/titleController.js
+++ app/js/controllers/titleController.js
@@ -3,12 +3,12 @@
 chrysalisApp.controller('TitleController', ['$scope', '$http', '$location', function($scope, $http, $location) {
 
   $http.get('data/navItems.json').success(function(navItemsData) {
+
     $scope.navItems = navItemsData;
-    console.log('navItems = ', $scope.navItems);
-    $scope.url = $location.$$url; // route with '/'
-    console.log('url = ', $scope.url);
-    // route name without '/' except for home
-    $scope.fixLink = function() {
+
+    $scope.url = $location.$$url; // route name with '/'
+
+    $scope.fixLink = function() { // route name without '/' except for home
       if ($scope.url === '/') {
         // for homepage
         $scope.link = '/';
@@ -17,34 +17,16 @@ chrysalisApp.controller('TitleController', ['$scope', '$http', '$location', func
       else {
         $scope.link = $scope.url.replace('/','');
       }
-      console.log('link = ', $scope.link);
     };
+
     $scope.fixLink();
+
     for (var i = 0; i < $scope.navItems.length; i++) {
-      console.log('navItems[' + i + '].link = ', $scope.navItems[i].link)
       if ($scope.navItems[i].link === $scope.link) {
         $scope.currentTitle = $scope.navItems[i].name;
-        console.log('currentTitle = ', $scope.currentTitle);
-        console.log('IF STATEMENT FIRED!!!')
-      }
-      else {
-        console.log('if statement did not fire');
       }
     }
 
   });
-
-  // change class to active when user clicks on nav links
-  $scope.currentPageCheck = function() {
-    $scope.itemSelected = this;
-    if ($scope.itemSelected.navItem.active === false) {
-      for (var i = 0; i < $scope.navItems.length; i++) {
-        if ($scope.navItems[i].active === true) {
-          $scope.navItems[i].active = false;
-        }
-      }
-      $scope.itemSelected.navItem.active = true;
-    }
-  };
 
 }]);
